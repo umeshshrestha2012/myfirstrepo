859063ddd95e2c70664b494164967ce69eeac104
Umesh Shrestha <umesh.shrestha2012@gmail.com>
2026-05-28 07:47:39 +0545

    added the new class

---
+++ src/test/java/javabasics/TestJava.java
@@ -0,0 +1,14 @@
+package javabasics;
+
+public class TestJava {
+    static void main() {
+        int a = 5;
+        int b = 15;
+
+        int sum = a+b;
+        System.out.println(sum);
+    }
+
+
+
+}
